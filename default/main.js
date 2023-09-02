e6f48724dc6d85faceecbb064fdb225f65c1de85
Jackson Xie <xjxxjx1017@gmail.com>
2023-09-02 22:39:20 +1200

    game saves

---
+++ default/main.js
@@ -0,0 +1,53 @@
+var roleHarvester = require('role.harvester');
+var roleUpgrader = require('role.upgrader');
+
+createRoles = function( role, count ){
+    var tmp = _.filter(Game.creeps, (creep) => creep.memory.role == role);
+    console.log('001 create: ' + tmp.length);
+
+    if(tmp.length < count) {
+        var newName = 'role' + Game.time;
+        console.log('002 Spawn: ' + newName);
+        Game.spawns['Spawn1'].spawnCreep([WORK,CARRY,MOVE], newName, 
+            {memory: {role: role}});
+    }
+}
+
+module.exports.loop = function () {
+
+    for(var name in Memory.creeps) {
+        if(!Game.creeps[name]) {
+            delete Memory.creeps[name];
+            console.log('Clearing non-existing creep memory:', name);
+        }
+    }
+
+    var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
+    console.log('Harvesters: ' + harvesters.length);
+
+    createRoles( 'harvester', 6 );
+    createRoles( 'upgrader', 2 );
+    createRoles( 'builder', 2 );
+    
+    if(Game.spawns['Spawn1'].spawning) { 
+        var spawningCreep = Game.creeps[Game.spawns['Spawn1'].spawning.name];
+        Game.spawns['Spawn1'].room.visual.text(
+            '🛠️' + spawningCreep.memory.role,
+            Game.spawns['Spawn1'].pos.x + 1, 
+            Game.spawns['Spawn1'].pos.y, 
+            {align: 'left', opacity: 0.8});
+    }
+
+    for(var name in Game.creeps) {
+        var creep = Game.creeps[name];
+        if(creep.memory.role == 'harvester') {
+            roleHarvester.run(creep);
+        }
+        if(creep.memory.role == 'upgrader') {
+            roleUpgrader.run(creep);
+        }
+        if(creep.memory.role == 'builder') {
+            roleBuilder.run(creep);
+        }
+    }
+}
